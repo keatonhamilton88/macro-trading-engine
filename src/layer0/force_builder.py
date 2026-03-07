e08dd6ef6bb2db79121047e4dcdf5b59867921cc
keatonhamilton88 <keatonhamilton88@gmail.com>
2026-03-06 21:55:54 -0700

    Implement ForceBuilder class for force calculations
    Added ForceBuilder class to build forces using z-score transformation.

--- src/layer0/force_builder.py
+++ src/layer0/force_builder.py
@@ -1,1 +1,16 @@
+import pandas as pd
+from src.features.transforms import z_score
 
+
+class ForceBuilder:
+
+    @staticmethod
+    def build_forces(sensor_df, lookback=252):
+
+        forces = pd.DataFrame(index=sensor_df.index)
+
+        for col in sensor_df.columns:
+            force_name = col.replace("_sensor", "_force")
+            forces[force_name] = z_score(sensor_df[col], lookback)
+
+        return forces
