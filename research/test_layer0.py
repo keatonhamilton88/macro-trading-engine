60e0ab8859dfe73a0c0c0c164f9d283f470265bd
keatonhamilton88 <keatonhamilton88@gmail.com>
2026-03-06 21:59:04 -0700

    Update test_layer0.py

--- research/test_layer0.py
+++ research/test_layer0.py
@@ -1,31 +1,22 @@
 from src.layer0.sensor_builder import SensorBuilder
-from src.layer0.force_table import ForceTableBuilder
-
+from src.layer0.force_builder import ForceBuilder
 
 builder = SensorBuilder()
 
-tickers = [
-    "HG=F",     # copper
-    "GC=F",     # gold
-    "CL=F",     # oil
+prices = builder.download_prices([
+    "HG=F",
+    "GC=F",
+    "CL=F",
     "SPY",
-    "TLT",
-    "HYG",
-    "LQD",
-    "EEM",
-    "AUDJPY=X",
     "^VIX"
-]
-
-prices = builder.download_prices(tickers)
+])
 
 sensors = builder.build_sensors(prices)
 
 print("\nSensors")
 print(sensors.tail())
 
+forces = ForceBuilder.build_forces(sensors)
 
-force_table = ForceTableBuilder.build(sensors)
-
-print("\nForce Table")
-print(force_table.tail())
+print("\nForces")
+print(forces.tail())
