5b631e4e0999b7d7df2952f51473c28b558e20a0
keatonhamilton88 <keatonhamilton88@gmail.com>
2026-03-06 20:47:10 -0700

    Integrate ForceTableBuilder and print force table
    Added ForceTableBuilder to test_layer0.py and printed force table.

--- research/test_layer0.py
+++ research/test_layer0.py
@@ -1,19 +1,19 @@
 from src.layer0.sensor_builder import SensorBuilder
+from src.layer0.force_table import ForceTableBuilder
 
 
 builder = SensorBuilder()
 
 tickers = [
-    "HG=F",   # copper
-    "GC=F",   # gold
-    "CL=F",   # oil
+    "HG=F",     # copper
+    "GC=F",     # gold
+    "CL=F",     # oil
     "SPY",
     "TLT",
     "HYG",
     "LQD",
     "EEM",
     "AUDJPY=X",
-    "DX-Y.NYB",
     "^VIX"
 ]
 
@@ -21,4 +21,11 @@ prices = builder.download_prices(tickers)
 
 sensors = builder.build_sensors(prices)
 
+print("\nSensors")
 print(sensors.tail())
+
+
+force_table = ForceTableBuilder.build(sensors)
+
+print("\nForce Table")
+print(force_table.tail())
