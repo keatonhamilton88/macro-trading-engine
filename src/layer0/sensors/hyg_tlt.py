bb03d202ad5c6987e4d572b928460829144fcd06
keatonhamilton88 <keatonhamilton88@gmail.com>
2026-03-08 21:59:24 -0600

    Update and rename hyg_lqd.py to hyg_tlt.py

--- src/layer0/sensors/hyg_tlt.py
+++ src/layer0/sensors/hyg_tlt.py
@@ -3,6 +3,6 @@ import numpy as np
 def credit_sensor(prices):
 
     hyg = prices["HYG"]
-    lqd = prices["LQD"]
+    lqd = prices["TLT"]
 
-    return np.log(hyg) - np.log(lqd)
+    return np.log(hyg) - np.log(tlt)
