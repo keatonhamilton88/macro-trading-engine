8c94cf6dbdb47897e105ff62c93945144c9cb019
keatonhamilton88 <keatonhamilton88@gmail.com>
2026-03-09 23:24:37 -0600

    Update put_call_ratio.py

--- src/layer0/sensors/put_call_ratio.py
+++ src/layer0/sensors/put_call_ratio.py
@@ -3,5 +3,5 @@ import numpy as np
 def put_call_ratio(prices):    
      if not prices.empty:
             return pd.Series(0.0, index=prices.index, name='put_call_ratio')
-        else:
-            return pd.Series(dtype=float)
+     else:
+       return pd.Series(dtype=float)
