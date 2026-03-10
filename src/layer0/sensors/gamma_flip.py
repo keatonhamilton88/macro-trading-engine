ca003afc09c18c8b492436dcda2d8cd2deec14ae
keatonhamilton88 <keatonhamilton88@gmail.com>
2026-03-09 23:22:10 -0600

    Update gamma_flip.py

--- src/layer0/sensors/gamma_flip.py
+++ src/layer0/sensors/gamma_flip.py
@@ -1,5 +1,7 @@
 import numpy as np
 
 def gamma_flip(prices):
-
-    return prices["gamma_flip"]
+    if not prices.empty:
+        return pd.Series(0.0, index=prices.index, name='gamma_flip')
+    else:
+        return pd.Series(dtype=float)
