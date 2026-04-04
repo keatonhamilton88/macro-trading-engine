045918c5362352cc2f370df72f4dec01a0470191
keatonhamilton88 <keatonhamilton88@gmail.com>
2026-04-03 23:32:35 -0600

    Update fxi.py

--- src/layer0/sensors/fxi.py
+++ src/layer0/sensors/fxi.py
@@ -1,5 +1,9 @@
 import numpy as np
 
 def fxi(prices):
-
-    return prices["FXI"]
+    if "FXI" not in prices:
+        return pd.Series(index=prices.index, dtype=float)
+    
+    fxi = prices["FXI"].ffill()
+    
+    return np.log(fxi)
