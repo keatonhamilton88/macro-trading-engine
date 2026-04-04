3bbe4d75a7d48512eb98f478c744e7eb99999254
keatonhamilton88 <keatonhamilton88@gmail.com>
2026-04-03 23:28:46 -0600

    Update eur_chf.py

--- src/layer0/sensors/eur_chf.py
+++ src/layer0/sensors/eur_chf.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 def eur_chf(prices):
+    if "EURCHF=X" not in prices:
+        return pd.Series(index=prices.index, dtype=float)
 
-    eurchf = prices["EURCHF=X"]
+    eurchf = prices["EURCHF=X"].ffill()
 
     return np.log(eurchf)
