e29f34fa65f9c8affeba7598a0d2cea8b3788e9b
keatonhamilton88 <keatonhamilton88@gmail.com>
2026-04-03 23:27:38 -0600

    Update eem_spy.py

--- src/layer0/sensors/eem_spy.py
+++ src/layer0/sensors/eem_spy.py
@@ -1,8 +1,10 @@
 import numpy as np
 
 def eem_spy(prices):
+    if "EEM" not in prices or "TLT" not in prices:
+        return pd.Series(index=prices.index, dtype=float)
 
-    eem = prices["EEM"]
-    spy = prices["SPY"]
+    eem = prices["EEM"].ffill()
+    spy = prices["SPY"].ffill()
 
     return np.log(eem) - np.log(spy)
