f68d65f5538aee5da270323d1f97db9612681e51
keatonhamilton88 <keatonhamilton88@gmail.com>
2026-04-03 23:49:34 -0600

    Update gc_si.py

--- src/layer0/sensors/gc_si.py
+++ src/layer0/sensors/gc_si.py
@@ -1,8 +1,10 @@
 import numpy as np
 
 def gc_si(prices):
+    if "GC=F" not in prices or "SI=F" not in prices:
+        return pd.Series(index=prices.index, dtype=float)
 
-    gc = prices["GC=F"]
-    si = prices["SI=F"]
+    gc = prices["GC=F"].ffill()
+    si = prices["SI=F"].ffill()
   
     return np.log(gc) - np.log(si)
