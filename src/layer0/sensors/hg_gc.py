d3e813b8384c90ae830a9344e3e3865ebd93b4aa
keatonhamilton88 <keatonhamilton88@gmail.com>
2026-04-03 23:51:18 -0600

    Update hg_gc.py

--- src/layer0/sensors/hg_gc.py
+++ src/layer0/sensors/hg_gc.py
@@ -1,8 +1,10 @@
 import numpy as np
 
 def hg_gc(prices):
+    if "HG=F" not in prices or "GC=F" not in prices:
+        return pd.Series(index=prices.index, dtype=float)
 
-    hg = prices["HG=F"]
-    gc = prices["GC=F"]
+    hg = prices["HG=F"].ffill()
+    gc = prices["GC=F"].ffill()
 
     return np.log(hg) - np.log(gc)
