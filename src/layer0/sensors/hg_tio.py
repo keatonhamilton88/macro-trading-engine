88880737ca806cf9eaa7cdd67c53536513166f4e
keatonhamilton88 <keatonhamilton88@gmail.com>
2026-04-03 23:51:55 -0600

    Update hg_tio.py

--- src/layer0/sensors/hg_tio.py
+++ src/layer0/sensors/hg_tio.py
@@ -1,8 +1,10 @@
 import numpy as np
 
 def hg_tio(prices):
+    if "HG=F" not in prices or "TIO=F" not in prices:
+        return pd.Series(index=prices.index, dtype=float)
 
-    hg = prices["HG=F"]
-    tio = prices["TIO=F"]
+    hg = prices["HG=F"].ffill()
+    tio = prices["TIO=F"].ffill()
 
     return np.log(hg) - np.log(tio)
