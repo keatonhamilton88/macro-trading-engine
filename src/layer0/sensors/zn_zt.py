e723938d1a98af1c5158d05dc1e1158814388e70
keatonhamilton88 <keatonhamilton88@gmail.com>
2026-04-04 01:48:46 -0600

    Update zn_zt.py

--- src/layer0/sensors/zn_zt.py
+++ src/layer0/sensors/zn_zt.py
@@ -1,8 +1,10 @@
 import numpy as np
 
-def zn_zt(prices):
+def zn_zt(prices):\
+    if "ZN=F" not in prices or "ZT=F" not in prices:
+        return pd.Series(index=prices.index, dtype=float)
 
-    zn = prices["ZN=F"]
-    zt = prices["ZT=F"]
+    zn = prices["ZN=F"].ffill()
+    zt = prices["ZT=F"].ffill()
 
     return np.log(zn) - np.log(zt)
