1d6cf1a51275cf6ad9aba84dc6703dcfcd0ae1e6
keatonhamilton88 <keatonhamilton88@gmail.com>
2026-04-03 23:04:08 -0600

    Update ashr_spy.py

--- src/layer0/sensors/ashr_spy.py
+++ src/layer0/sensors/ashr_spy.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 def ashr_spy(prices):
+  if "ASHR" not in prices or "SPY" not in prices:
+        return pd.Series(index=prices.index, dtype=float)
 
   ashr = prices["ASHR"]
   spy = prices["SPY"]
