d9df4469e47d80942a65f55155f30ade203c9534
keatonhamilton88 <keatonhamilton88@gmail.com>
2026-04-03 23:07:39 -0600

    Update aud_jpy.py

--- src/layer0/sensors/aud_jpy.py
+++ src/layer0/sensors/aud_jpy.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 def aud_jpy(prices):
+    if "AUD" not in prices or "JPY" not in prices:
+        return pd.Series(index=prices.index, dtype=float)
 
     audjpy = prices["AUDJPY=X"]
 
