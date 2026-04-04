d16d300808a59c1015fcb9089b3c06190d239412
keatonhamilton88 <keatonhamilton88@gmail.com>
2026-04-03 20:22:05 -0600

    Update data_loader.py

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -3,5 +3,9 @@ import yfinance as yf
 class YFinanceLoader:
 
     def load_close(self, symbol, period="5y"):
-        data = yf.download(symbol, period=period)
-        return data['Close']
+        data = yf.download(symbol, period=period, auto_adjust=True)
+
+        if isinstance(data.columns, pd.MultiIndex):
+            data = data.xs("Close", level=0, axis=1)
+        
+        return data
