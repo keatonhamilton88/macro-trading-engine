6fd3ea8cdb46f1cf6aab42c6de8bd68da27d654e
keatonhamilton88 <keatonhamilton88@gmail.com>
2026-04-04 01:26:26 -0600

    Update vix.py

--- src/layer0/sensors/vix.py
+++ src/layer0/sensors/vix.py
@@ -1,6 +1,6 @@
 def vix(prices):
     if not prices is None or prices.empty:
-        return pd.Series(0.0, index=prices.index, name='spx_gex')
+        return pd.Series(0.0, index=prices.index, name='vix')
     else:
         return pd.Series(dtype=float)
 
