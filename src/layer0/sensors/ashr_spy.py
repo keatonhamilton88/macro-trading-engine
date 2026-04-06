ff5da8faa4839df30c07a45a76b35bbe4a76e385
keatonhamilton88 <keatonhamilton88@gmail.com>
2026-04-05 20:27:00 -0600

    Implement None checks for input columns in compute function
    Add checks for None values in ashr_col and spy_col.

--- src/layer0/sensors/ashr_spy.py
+++ src/layer0/sensors/ashr_spy.py
@@ -1,12 +1,12 @@
 import numpy as np
+import pandas as pd 
 
 def compute(prices, ashr_col, spy_col):
-    # The 'Builder' already checked if these exist
+    if ashr_col is None or spy_col is None:
+        return pd.Series(index=prices.index, dtype=float)
+    
     ashr = prices[ashr_col].ffill()
     spy = prices[spy_col].ffill()
-    
     return np.log(ashr) - np.log(spy)
-
-
 
   
